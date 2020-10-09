49fcbcdaf04227e0599e08ba68bc7d6164223c71
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-09 09:10:23 -0700

    PASS test for project.all when no projects exist

--- lib/project.rb
+++ lib/project.rb
@@ -12,7 +12,14 @@ class Project
   end
 
   def self.all
-    
+    projects = []
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    returned_projects.each do |project|
+      title = project.fetch('title')
+      id = project.fetch('id').to_i
+      projects << Project.new({title: title, id: it})
+    end
+    projects
   end
 
 end
