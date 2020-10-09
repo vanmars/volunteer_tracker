571361d6d2a005523c2690721a6e93771d162875
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-09 08:58:56 -0700

    PASS test for project.title method

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,10 @@
 class Project
-  attr_reader
-  attr_accessor
+  attr_reader :id
+  attr_accessor :title
 
-  def initialize
-    
+  def initialize(attributes)
+    @title = attributes.fetch(:title)
+    @id = attributes.fetch(:id)
   end
 
 end
