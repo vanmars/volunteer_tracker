a3a9dc9fcea65166114b0a9e63ae716d3d7776d4
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-09 09:50:27 -0700

    PASS test for volunteer.id method (before a volunteer is added)

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,11 +1,11 @@
 class Volunteer
-  attr_reader
+  attr_reader :id
   attr_accessor :name, :project_id
 
   def initialize(attributes)
     @name = attributes.fetch(:name)
     @project_id = attributes.fetch(:project_id)
-    @id
+    @id = attributes.fetch(:id)
   end
 
 end
