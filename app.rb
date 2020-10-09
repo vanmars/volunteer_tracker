f881341c43fa73448340c7cefb08dcc2af679502
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-09 10:45:48 -0700

    Good fail for integration test 2: Project update path

--- app.rb
+++ app.rb
@@ -9,4 +9,25 @@ require("pg")
 DB = PG.connect({:dbname => "volunteer_tracker"})
 
 get('/') do
+  redirect to('/projects')
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+post('/projects') do
+  title = params[:title]
+  new_project = Project.new({title: title, id: nil})
+  new_project.save
+  redirect to('/projects')
+end
+
+get('/projects/:id') do
+  erb(:project)
+end
+
+patch('/projects/:id') do
+  redirect to('/projects/:id')
 end
