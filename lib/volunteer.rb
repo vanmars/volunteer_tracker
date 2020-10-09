681e4453a34a5c84da019cc7d58a359644d4aa98
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-09 12:07:40 -0700

    PASS test for volunteer.update

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -50,5 +50,9 @@ class Volunteer
   end
 
   def update(attributes)
+    if attributes.has_key?(:name) && attributes.fetch(:name) != nil
+      @name = attributes.fetch(:name)
+      DB.exec("UPDATE volunteers SET name = '#{@name}' WHERE id = #{@id};")
+    end
   end
 end
