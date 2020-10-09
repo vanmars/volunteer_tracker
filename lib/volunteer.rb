8ac8243c463c6e0ec4b7e0835954a8b33f0770cd
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-09 10:04:11 -0700

    PASS test for volunteer.save method

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -31,7 +31,8 @@ class Volunteer
   end
 
   def save
-    
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
+    @id = result.first.fetch('id').to_i
   end
 
 end
