7cb6d397fb0ffc55def8a343471f566af57400f7
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-09 09:44:49 -0700

    PASS test for volunteer.project_id method

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,10 +1,10 @@
 class Volunteer
   attr_reader
-  attr_accessor :name
+  attr_accessor :name, :project_id
 
   def initialize(attributes)
     @name = attributes.fetch(:name)
-    @project_id
+    @project_id = attributes.fetch(:project_id)
   end
 
 end
